fa7aaf6ab261555c687197012b81515f7605a620
Maria Magdalini <momoloca42@gmail.com>
2023-07-07 16:10:04 +0300

    add: deploy workflow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ return (
         <div id="nav" className="w-full flex items-center font-['Acme'] z-50
         transition duration-300 ease-out justify-around bg-gray-500 h-[100px]">
             <div className="">
-                <p className="text-yellow-50 text-[20px]" id='shop'><a href="/">Samurai's Shop</a></p>
+                <p className="text-yellow-50 text-[20px]" id='shop'><a href="/vite/">Samurai's Shop</a></p>
             </div>
             <div className=" h-[100px] w-[50px] right-0  top-0 absolute"  id="burger">
              <span className="w-6 h-[3px] rounded-md mt-10 absolute  bg-black "></span>
